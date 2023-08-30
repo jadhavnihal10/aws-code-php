8d92176d23dd35d877f7239ebec550284c663485
Nihal Jadhav <89071571+jadhavnihal10@users.noreply.github.com>
2023-08-30 18:21:11 +0530

    Create phpcode.php

---
+++ phpcode.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP Example</title>
+</head>
+<body>
+    <h1>Welcome to our PHP Example Page</h1>
+    
+    <?php
+    $name = "Nihal";
+    $age = 29;
+    ?>
+    
+    <p>Hello, my name is <?php echo $name; ?> and I am <?php echo $age; ?> years old.</p>
+    
+    <p>Today's date is <?php echo date("F j, Y"); ?>.</p>
+</body>
+</html>
